26e8780341102f033a52c74804265467b84897d9
NqobileNgobeni <115787995+NqobileNgobeni@users.noreply.github.com>
2024-05-16 09:18:32 +0200

    v

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/dist/index.html",
   plugins: [react()],
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+  },
 });
